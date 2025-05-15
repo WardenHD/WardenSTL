adbcbb976fa11ec585506d1ba04b56eadac3b204
WardenHD <tomas.bezruchko@gmail.com>
2025-05-15 23:31:43 +0300

    Added license header

--- include/wstl/Memory.hpp
+++ include/wstl/Memory.hpp
@@ -1,3 +1,13 @@
+// Part of WardenSTL - https://github.com/WardenHD/WardenSTL
+// Copyright (c) 2025 Artem Bezruchko (WardenHD)
+//
+// This file is based on the Embedded Template Library (ETL)'s memory.h,
+// which are licensed under the MIT License.
+//
+// It has been restructured and adapted for WardenSTL.
+//
+// Licensed under the MIT License. See LICENSE file for details.
+
 #ifndef __WSTL_MEMORY_HPP__
 #define __WSTL_MEMORY_HPP__
 
