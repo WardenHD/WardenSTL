88605024afdd32d8bcd92eab76310edc5d2e5df5
WardenHD <tomas.bezruchko@gmail.com>
2025-06-06 00:37:09 +0300

    Added license headers

--- include/wstl/PlacementNew.hpp
+++ include/wstl/PlacementNew.hpp
@@ -1,3 +1,8 @@
+// Part of WardenSTL - https://github.com/WardenHD/WardenSTL
+// Copyright (c) 2025 Artem Bezruchko (WardenHD)
+//
+// Licensed under the MIT License. See LICENSE file for details.
+
 #ifndef __WSTL_PLACEMENTNEW_HPP__
 #define __WSTL_PLACEMENTNEW_HPP__
 
@@ -5,14 +10,10 @@
 #include <stddef.h>
 
 
-// Defines the placement new operators if they are not already defined
-
-#ifndef __WSTL_CUSTOM_NEW__
 inline void* operator new(size_t, void* p) __WSTL_NOEXCEPT__ { return p; }
 inline void* operator new[](size_t, void* p) __WSTL_NOEXCEPT__ { return p; }
 
 inline void operator delete(void*, void*) __WSTL_NOEXCEPT__ {}
 inline void operator delete[](void*, void*) __WSTL_NOEXCEPT__ {}
-#endif
 
 #endif
