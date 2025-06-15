9256df391a6c9ca708ff88f107282a35a3334292
WardenHD <tomas.bezruchko@gmail.com>
2025-06-16 01:25:31 +0300

    Starting implementing a pool of objects

--- include/wstl/Pool.hpp
+++ include/wstl/Pool.hpp
@@ -2,27 +2,35 @@
 #define __WSTL_POOL_HPP__
 
 #include "private/Platform.hpp"
+#include "Container.hpp"
+#include "PlacementNew.hpp"
+#include "StandardExceptions.hpp"
+#include "NullPointer.hpp"
+#include "private/Error.hpp"
 #include <stddef.h>
 
 
 
 namespace wstl {
-    class IPool {
+    template<typename T, const size_t SIZE>
+    class Pool : public TypedContainerBase<T> {
     public:
-        typedef size_t SizeType;
-        
+        typedef typename TypedContainerBase<T>::ValueType ValueType;
+        typedef typename TypedContainerBase<T>::SizeType SizeType;
+        typedef typename TypedContainerBase<T>::DifferenceType DifferenceType;
+        typedef typename TypedContainerBase<T>::ReferenceType ReferenceType;
+        typedef typename TypedContainerBase<T>::ConstReferenceType ConstReferenceType;
+        typedef typename TypedContainerBase<T>::PointerType PointerType;
+        typedef typename TypedContainerBase<T>::ConstPointerType ConstPointerType;
 
-    protected:
-        IPool(char* buffer, SizeType itemSize, SizeType capacity) :
-            m_Buffer(buffer), m_CurrentSize(0), m_ItemsInUse(0), 
-            m_ItemSize(itemSize), m_Capacity(capacity) {}
+
+        T* Allocate() {
+            __WSTL_ASSERT_RETURNVALUE__(!this->Full(), LengthError("Pool is full"), NullPointer);
+        }
     
     private:
-        char* m_Buffer;
-        SizeType m_CurrentSize;
-        SizeType m_ItemsInUse;
-        const SizeType m_ItemSize;
-        const SizeType m_Capacity;
+        T m_Buffer[SIZE];
+        SizeType m_ItemsInitialized;
     };
 }
 
