090552cad7c66c491b67d3e0359c47726ac8e797
WardenHD <tomas.bezruchko@gmail.com>
2025-08-05 02:36:03 +0300

    Made n variable constexpr in C++14

--- include/wstl/Bit.hpp
+++ include/wstl/Bit.hpp
@@ -848,7 +848,7 @@ namespace wstl {
     /// @see https://en.cppreference.com/w/cpp/numeric/rotl
     template<typename T>
     __WSTL_CONSTEXPR14__ typename EnableIf<IsUnsigned<T>::Value, T>::Type RotateLeft(T value, int shift) __WSTL_NOEXCEPT__ {
-        const int n = NumericLimits<T>::Digits;
+        const __WSTL_CONSTEXPR14__ int n = NumericLimits<T>::Digits;
         const int r = ((shift % n) + n) % n;
         return (value << r) | (value >> (n - r));
     }
