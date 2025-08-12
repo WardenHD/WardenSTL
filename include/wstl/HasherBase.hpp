e05d42cf56899ad05ac38a51e43cf8cf57542579
WardenHD <tomas.bezruchko@gmail.com>
2025-08-12 04:00:22 +0300

    Added documentation and removed all constructors except default

--- include/wstl/HasherBase.hpp
+++ include/wstl/HasherBase.hpp
@@ -1,3 +1,8 @@
+// Part of WardenSTL - https://github.com/WardenHD/WardenSTL
+// Copyright (c) 2025 Artem Bezruchko (WardenHD)
+//
+// Licensed under the MIT License. See LICENSE file for details.
+
 #ifndef __WSTL_HASHERBASE_HPP__
 #define __WSTL_HASHERBASE_HPP__
 
@@ -6,6 +11,11 @@
 
 
 namespace wstl {
+    /// @brief Base class for hashers
+    /// @tparam Derived The derived class that implements the hashing logic
+    /// @tparam THash The type of the hash value, typically an unsigned integer type
+    /// @tparam TValue The type of the value being hashed, typically an unsigned integer type
+    /// @ingroup hash
     template<typename Derived, typename THash, typename TValue>
     class HasherBase {
     public:
@@ -14,43 +24,61 @@ namespace wstl {
         typedef TValue ValueType;
 
         StaticAssert(IsUnsigned<ValueType>::Value && IsUnsigned<HashType>::Value, "Unsigned types in derived class are not supported");
-
+        
+        /// @brief Pushes a value into the hasher
+        /// @details This method must be implemented by the derived class
+        /// @param value The value to be hashed
         __WSTL_CONSTEXPR14__ void PushBack(ValueType value) {
             static_cast<Derived*>(this)->PushBack(value);
         }
 
+        /// @brief Appends a range to the hasher
+        /// @param first The beginning of the range
+        /// @param last The end of the range
         template<typename Iterator>
         __WSTL_CONSTEXPR14__ void Append(Iterator first, Iterator last) {
             for(; first != last; ++first) PushBack(*first);
         }
 
         #ifdef __WSTL_CXX11__
+        /// @brief Appends a container to the hasher
+        /// @param container The container whose elements will be hashed
         template<typename Container>
         __WSTL_CONSTEXPR14__ void Append(Container&& container) {
             Append(Begin(container), End(container));
         }
         #else
-
+        /// @brief Appends a container to the hasher
+        /// @param container The container whose elements will be hashed
         template<typename Container>
         void Append(const Container& container) {
             Append(Begin(container), End(container));
         }
         #endif
 
+        /// @brief Resets the hasher to its initial state
+        /// @details This method must be implemented by the derived class
         __WSTL_CONSTEXPR14__ void Reset() {
             static_cast<Derived*>(this)->Reset();
         }
 
+        /// @brief Finalizes and gets the final hash value
+        /// @details The `Finalize` method must be implemented by the derived class
+        /// @return The final hash value
         __WSTL_CONSTEXPR14__ HashType Value() {
             static_cast<Derived*>(this)->Finalize();
             return m_Hash;
         }
 
+        /// @brief Conversion operator to get the final hash value
+        /// @details The `Finalize` method must be implemented by the derived class
+        /// @return The final hash value
         __WSTL_CONSTEXPR14__ operator HashType() {
             static_cast<Derived*>(this)->Finalize();
             return m_Hash;
         }
-
+        
+        /// @brief Returns a back insert iterator for the hasher
         __WSTL_CONSTEXPR14__ InsertIterator Inserter() {
             return InsertIterator(*static_cast<Derived*>(this));
         }
@@ -58,16 +86,8 @@ namespace wstl {
     protected:
         HashType m_Hash;
 
-        __WSTL_CONSTEXPR14__ HasherBase() {
-            Reset();
-        }
-
-        template<typename Iterator>
-        __WSTL_CONSTEXPR14__ HasherBase(Iterator first, Iterator last) {
-            StaticAssert(sizeof(typename IteratorTraits<Iterator>::ValueType) == sizeof(ValueType), "Type not supported");
-            Reset();
-            Append(first, last);
-        }
+        /// @brief Default constructor
+        __WSTL_CONSTEXPR14__ HasherBase() {}
     };
 }
 
